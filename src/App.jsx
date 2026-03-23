76e323c239091199bbc9d6433d974a3efca5deae
Benita Almroth <benita.almroth@hotmail.com>
2026-03-23 13:35:30 +0100

    added base html for choose role, started css

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,24 @@ import { ChooseRole } from "./components/ChooseRole";
 function App() {
   return (
     <>
-      <ChooseRole title="Student" description="I'm here to learn and connect" />
+      <section className="roles">
+        <div className="roleInfo">
+          <h4>Register to</h4>
+          <h1>Stalk the people at the event</h1>
+          <h4>I AM A...</h4>
+        </div>
 
-      <ChooseRole title="Industry" description="I'm here to meet talent" />
+        <ChooseRole
+          title="Student"
+          description="I'm here to learn and connect"
+        />
+
+        <ChooseRole title="Industry" description="I'm here to meet talent" />
+      </section>
+
+      <section>
+        {/* create profile logic */}
+      </section>
     </>
   );
 }
