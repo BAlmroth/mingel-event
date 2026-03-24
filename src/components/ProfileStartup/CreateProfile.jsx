7724883966d1e92c78c7ffc30ca55624ed49ba40
Benita Almroth <benita.almroth@hotmail.com>
2026-03-24 10:57:27 +0100

    excerted top header into its own component

--- src/components/ProfileStartup/CreateProfile.jsx
+++ src/components/ProfileStartup/CreateProfile.jsx
@@ -1,27 +1,23 @@
 import { useSearchParams } from "react-router-dom";
 import Styles from "./CreateProfile.module.css";
+import { PageHeader } from "./StartupHeader";
 
 export function CreateProfile() {
   const [searchParams] = useSearchParams();
   const role = searchParams.get("role");
   const isStudent = role === "student";
-  
+
   const handleSubmit = (e) => {
     e.preventDefault(); //hoppa inte till en ny sida
   };
 
   return (
     <section>
-      <div className={Styles.profileInfo}>
-        <h4>Register to</h4>
-        <h1>Create Profile</h1>
-      </div>
-      <div>
-        <p>Selected role: {role}</p>
-      </div>
+        
+        <PageHeader/>
 
       <form className={Styles.inputInfo} onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
+        <label htmlFor="name">Full name</label>
         <input id="name" name="name" type="text" placeholder="Your name" />
 
         <label htmlFor="company">{isStudent ? "Program" : "Company"}</label>
---
+++ src/components/ProfileStartup/StartupHeader.jsx
@@ -0,0 +1,11 @@
+import styles from "./StartupHeader.module.css";
+
+export function PageHeader() {
+  return (
+    <div className={styles.roleInfo}>
+      <h4>Register to</h4>
+      <h1>Stalk the people at the event</h1>
+      <h4>I AM A...</h4>
+    </div>
+  );
+}
