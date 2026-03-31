d6a0a1c3029a1232335250b0b7652537c761ed48
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-03-31 11:47:32 +0200

    finished ui for profile page

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import supabase from "../../lib/supabaseClient";
 import styles from "./MyProfile.module.css"
+import { QRCodeCanvas } from "qrcode.react"
+import Star from "../../assets/Star.svg"
+
+
 
 export function MyProfile() {
   const [user, setUser] = useState(null);
@@ -51,6 +55,8 @@ export function MyProfile() {
 
   if (!user) return <p>Loading...</p>;
 
+  const linkedinUrl = `https://www.linkedin.com/in/${user.linkedin_id}`;
+
 
   return (
     <section className={styles.profilesection}>
@@ -60,15 +66,23 @@ export function MyProfile() {
       </div>
       <div className={styles.avatar}>
         {user.picture ? (
-        <img src={user.picture} alt="profile" style={{ width: "150px", borderRadius: "50%" }} />
+        <img src={user.picture} alt="profile" style={{ width: "150px", borderRadius: "50%", marginBottom: "1.5rem" }} />
         ) : (
         getInitials(`${user.first_name} ${user.last_name}`)
   )}
 
-      <h2>{user.first_name} {user.last_name}</h2>
-      <p>
-        {user.role} • {user.description}
-      </p>
+          <h2>{user.first_name} {user.last_name}</h2>
+          <p>
+            {user.role} • {user.description}
+          </p>
+        <div className={styles.qrContainer}>
+              <QRCodeCanvas value={linkedinUrl} size={140} />
+          </div>
+          <p>https://www.linkedin.com/in/${user.linkedin_id}</p>
+      </div>
+      <div className={styles.stalkLater}>
+        <img src={Star} alt="stalk" className={styles.stalkBtn} />
+        <h3>Stalk for later</h3>
       </div>
     </section>
   );
