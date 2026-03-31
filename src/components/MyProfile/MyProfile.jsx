9d4a7184b94c75af09be52f2faeb92ebb462e1d3
Benita Almroth <benita.almroth@hotmail.com>
2026-03-31 10:43:32 +0200

    put all db fetch logic for frontend in context hook

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -1,47 +1,11 @@
-import { useEffect, useState } from "react";
-import supabase from "../../lib/supabaseClient";
-import styles from "./MyProfile.module.css"
+import { useUser } from "../../hooks/UserContext";
+import styles from "./MyProfile.module.css";
 
 export function MyProfile() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      const res = await fetch("http://localhost:4000/me", {
-        credentials: "include",
-      });
-
-      if (!res.ok) return;
-
-      const data = await res.json();
-
-    
-
-      if (data) {
-        fetchUserFromDB(data);
-      }
-    };
-
-    fetchUser();
-  }, []);
-
-  const fetchUserFromDB = async (linkedinUser) => {
-    const fullName = `${linkedinUser.first_name} ${linkedinUser.last_name}`;
-
-    const { data, error } = await supabase
-      .from("users")
-      .select("*")
-      .eq("linkedin_id", linkedinUser.linkedin_id)
-      .maybeSingle();
-
-    if (!error) {
-      setUser(data);
-    }
-  };
+  const { user, loading } = useUser();
 
   const getInitials = (name) => {
     if (!name) return "";
-
     return name
       .split(" ")
       .map((n) => n[0])
@@ -49,23 +13,21 @@ export function MyProfile() {
       .toUpperCase();
   };
 
-  if (!user) return <p>Loading...</p>;
+  if (loading) return <p>Loading...</p>;
+  if (!user) return <p>Not logged in</p>;
 
-
-  return (
-    <section>
-      <div className={styles.avatar}>
-        {user.picture ? (
+return (
+  <section>
+    <div className={styles.avatar}>
+      {user.picture ? (
         <img src={user.picture} alt="profile" />
-        ) : (
+      ) : (
         getInitials(`${user.first_name} ${user.last_name}`)
-  )}
-</div>
+      )}
+    </div>
 
-      <h2>{user.first_name} {user.last_name}</h2>
-      <p>
-        {user.role} • {user.description}
-      </p>
-    </section>
-  );
+    <h2>{user.first_name} {user.last_name}</h2>
+    <p>{user.role} • {user.description}</p>
+  </section>
+);
 }
