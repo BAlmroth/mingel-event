fe1cef25de97dcd7e2c3e992740f0cea323da25f
Benita Almroth <benita.almroth@hotmail.com>
2026-04-01 13:31:07 +0200

    fixed server and env to match deployed site, added Procfile for herpku

--- server/index.js
+++ server/index.js
@@ -15,10 +15,13 @@ import cors from "cors";
 import linkedinAuth from "./linkedinAuth.js";
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ 
+  origin: process.env.CLIENT_URL || "http://localhost:5173", 
+  credentials: true 
+}));
 
 //session
 app.use(
