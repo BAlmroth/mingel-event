f026f285b5551c46e264e7108798df4d8a5da3cf
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-04-09 13:23:04 +0200

    Added if.statement when logged in ad imported svg-icons

--- src/components/Navigation/BottomNav.jsx
+++ src/components/Navigation/BottomNav.jsx
@@ -1,11 +1,13 @@
 import { useLocation, useNavigate } from "react-router-dom"
 import styles from "./BottomNav.module.css"
 import joinBtn from "../../assets/joinBtn.svg"
+import { useUser } from "../../hooks/UserContext"
 
 
 export function BottomNav() {
     const navigate = useNavigate()
     const location = useLocation()
+    const { user } = useUser()
 
     return (
         <nav className={styles.nav}>
@@ -16,15 +18,17 @@ export function BottomNav() {
           <p>Mingle Feed</p>
         </div>
     
+        {!user && (
         <div className={styles.joinWrapper}>
-            <img
-                src={joinBtn}
-                className={styles.joinSvg}
-                onClick={() => navigate("/")}
-                alt="join"
-            />
-            <p className={styles.joinText}>Join</p>
-        </div>      
+          <img
+            src={joinBtn}
+            className={styles.joinSvg}
+            onClick={() => navigate("/")}
+            alt="join"
+          />
+          <p className={styles.joinText}>Join</p>
+        </div>
+      )}   
     
         <div
           className={location.pathname === "/profile" ? styles.active : ""}
